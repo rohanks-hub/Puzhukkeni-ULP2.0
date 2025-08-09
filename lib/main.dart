5075551e35ec2b954321e57146d0030844de1058
rohanks-hub <rohanksojo9c@gmail.com>
2025-08-09 12:04:28 +0530

    alrm core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'app/main_app.dart';
+import 'app/services/alarm_service.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await AlarmService.initialize();
   runApp(const MainApp());
 }
